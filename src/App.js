e78c751679d5e634966ff88e45a2c9cd8d6e06b7
Dimterion <dimterion59@gmail.com>
2022-12-13 11:20:47 +0100

    Added routes, favicon.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Posts from "./pages/Posts";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+
 function App() {
   return (
     <>
-      <h1>My App</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,5 @@
+function SignIn() {
+  return <div>Sign In</div>;
+}
+
+export default SignIn;
