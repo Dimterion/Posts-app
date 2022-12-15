fc9a273138ed2747d1dee8079c0343a6a0dd19af
Dimterion <dimterion59@gmail.com>
2022-12-15 10:47:56 +0100

    Added private route and spinner.

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStatus } from "../hooks/useAuthStatus";
+import Spinner from "./Spinner";
+
+export const PrivateRoute = () => {
+  const { loggedIn, checkingStatus } = useAuthStatus();
+
+  if (checkingStatus) {
+    return <Spinner />;
+  }
+
+  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
+};
